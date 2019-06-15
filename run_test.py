7373a681876c4f93a1c9dd206b1bbb219a5a4749
任明 <renming@jiekecloud.com>
2019-06-15 19:03:21 +0800

    修改报告名称格式

--- run_test.py
+++ run_test.py
@@ -30,7 +30,7 @@ def testRun(project,env):
     file = unittest.defaultTestLoader.discover(test_dir, pattern='*_test.py')  # 匹配结尾为test的py文件
     now = time.strftime("%Y%m%d%H%M", time.localtime(time.time()))  # 取当前时间
     public_path = os.path.dirname(os.path.abspath(sys.argv[0])) # 获取当前运行的.py文件所在的绝对路径
-    filename = public_path + "/Report/" + project+ "-" + env+ "-" + now + "-report.html"   #保存的报告路径和名称
+    filename = public_path + "/Report/" +  now + "-" + project+ "-" + env + "-report.html"   #保存的报告路径和名称
     # reportName = now + " report.html"
     fp = open(filename, 'wb')
     # print("fp is :",fp)
