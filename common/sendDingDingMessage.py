3f8f1adbc2ae96101f00d90c038176256e5ab1ad
任明 <renming@jiekecloud.com>
2019-07-13 18:23:42 +0800

    url

--- common/sendDingDingMessage.py
+++ common/sendDingDingMessage.py
@@ -9,8 +9,10 @@ import requests,json
 
 
 def ddRobot(message):
-    # url = 'https://oapi.dingtalk.com/robot/send?access_token=9e4321caf5329425fe869ec76d5501770c90cd526b1cb805834cc01d34bf0ba5'
-    url = 'https://oapi.dingtalk.com/robot/send?access_token=df8d1780fa8ca96c11c95573f452e36729080e2036dc81b390922b06a2606095'
+    #研发中心
+    url = 'https://oapi.dingtalk.com/robot/send?access_token=9e4321caf5329425fe869ec76d5501770c90cd526b1cb805834cc01d34bf0ba5'
+    #测试组内部
+    # url = 'https://oapi.dingtalk.com/robot/send?access_token=df8d1780fa8ca96c11c95573f452e36729080e2036dc81b390922b06a2606095'
     HEADERS = {"Content-Type": "application/json;charset=utf-8"}
 
     String_textMsg = {
