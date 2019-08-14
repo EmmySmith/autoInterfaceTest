c6c38dbb6cfa46c8ec837c0b5c4ce947c58f9527
任明 <renming@jiekecloud.com>
2019-08-14 10:48:08 +0800

    删除废弃接口

--- common/sendDingDingMessage.py
+++ common/sendDingDingMessage.py
@@ -10,11 +10,11 @@ import requests,json
 
 def ddRobot(message):
     #研发中心
-    # url = 'https://oapi.dingtalk.com/robot/send?access_token=9e4321caf5329425fe869ec76d5501770c90cd526b1cb805834cc01d34bf0ba5'
+    url = 'https://oapi.dingtalk.com/robot/send?access_token=9e4321caf5329425fe869ec76d5501770c90cd526b1cb805834cc01d34bf0ba5'
     #测试组内部
     # url = 'https://oapi.dingtalk.com/robot/send?access_token=df8d1780fa8ca96c11c95573f452e36729080e2036dc81b390922b06a2606095'
     #调试用
-    url = 'https://oapi.dingtalk.com/robot/send?access_token=c08b936af7bc08a69a9fc347099ada832059c999736dfd42a9bb8670d6af3dbb'
+    # url = 'https://oapi.dingtalk.com/robot/send?access_token=c08b936af7bc08a69a9fc347099ada832059c999736dfd42a9bb8670d6af3dbb'
 
     HEADERS = {"Content-Type": "application/json;charset=utf-8"}
 
