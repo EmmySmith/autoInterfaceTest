7f8ba4efbee173dd758397d7456feb6957a8a3ad
zhangminSn <zhangmin@jiekecloud.com>
2019-09-07 14:48:09 +0800

    修改保存图片素材的group_id

--- test_interface/CAP/material/savematerial_test.py
+++ test_interface/CAP/material/savematerial_test.py
@@ -11,6 +11,7 @@ import unittest
 import time,random
 from common.public import *
 from common.commonData import *
+from mysqlHandle.common_mysql import *
 
 class CAP_Interface(unittest.TestCase):
 
@@ -18,6 +19,8 @@ class CAP_Interface(unittest.TestCase):
     def setUpClass(self):
         self.headers = headers
         self.host = host
+        self.sql1 ='select group_id from t_material where is_deleted=0 ORDER BY  id desc LIMIT 1;'
+        self.dbname = "geek_icem_component"
         self.path = "/api/icem-component/material/save?appid=2019031863584181"
         print("----------开始测试----------")
 
@@ -25,10 +28,12 @@ class CAP_Interface(unittest.TestCase):
     def test_savemicropage(self):
         """保存图片素材"""
         self.url = self.host + self.path
+        self.group_id = str(DB_ICEM_proc(self.dbname).get_vslues(self.sql1))
+
         data = {
             "type": "image",
             "title": "9469669_142840860000_2.jpg",
-            "group_id": 34,
+            "group_id": self.group_id,
             "url": "https://geek-icem.oss-cn-beijing.aliyuncs.com/release/1000/material/d1db5171c5b64e5291026c40d9f97afe.jpg"
         }
 
